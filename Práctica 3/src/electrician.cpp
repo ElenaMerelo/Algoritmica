c44ee0d88155e7fd54318689ce74f87a4b84b089
antoniogamiz <antoniogamiz10@gmail.com>
2018-04-24 17:47:20 +0200

    Eliminado codigo de sobra

--- Práctica 3/src/electrician.cpp
+++ Práctica 3/src/electrician.cpp
@@ -15,22 +15,6 @@ using namespace std;
 
 #define BUFFERSIZE 50
 
-struct edge
-{
-    struct node p1;
-    struct node p2;
-    float weight;
-
-    edge(){};
-
-    edge(struct node p1_, struct node p2_, float weight_)
-    {
-        p1=p1_;
-        p2=p2_;
-        weight=weight_;
-    }
-};
-
 float euclidean_distance(pair<int, int> x1, pair<int, int> x2){ return sqrt( pow(x1.first-x2.first,2) + pow(x1.second-x2.second,2) ); }
 
 int get_dimension(ifstream &f)
@@ -78,9 +62,6 @@ void fill_graph(graph &g, ifstream &f)
     }
 }
 
-
-bool compare_by_weight(const edge& a, const edge& b){ return a.weight < b.weight; }
-
 bool is_in(int j, vector<node>& v)
 {
     for(int i=0; i<v.size(); i++)
@@ -92,40 +73,6 @@ bool is_in(int j, vector<node>& v)
 }
 
 bool cycle(vector<node>& v, struct node new_node){ return is_in(new_node.label, v); }
-
-vector<edge> generate_all_edges(graph &g)
-{
-    vector<node> nodes=g.get_nodes();
-    vector<edge> edges;
-
-    for(int k=0; k<g.size()-1; k++)
-        for(int i=0; i<g.size(); i++)
-            for(int j=i+k+1; j<g.size(); j++)
-                edges.push_back(edge(nodes[i],nodes[j],g.get_weight(i,j)));
-
-    return edges;
-
-}
-
-// vector<edge> kruskal(graph &g)
-// {
-//     //First we create a vector with each edge of the grap.
-//     vector<edge> edges=generate_all_edges(g);
-
-//     sort(edges.begin(), edges.end(), compare_by_weight); //Sort the vector of edges.
-
-//     vector<edge> solution;
-
-//     while(edges.size())
-//     {
-//         if( !cycle(solution, edges[0]) )
-//             solution.push_back(edges[0]);
-
-//         edges.erase(edges.begin());
-//     }
-
-//     return solution;
-// }
 
 int nodeHeuristic(graph &g, struct node n, float min_bound, vector<node> taken){
     float min=LONG_MAX;
@@ -144,47 +91,6 @@ int nodeHeuristic(graph &g, struct node n, float min_bound, vector<node> taken){
 
     return index_min;
 }
-
-// vector<node> kruskal_heuristic(graph &g)
-// {
-//     vector<edge> solution;    
-//     vector<node> nodes=g.get_nodes();
-
-//     struct node init=nodes[2]; //Heuristic starts with the first node on the file.
-
-//     int nextNode, aux;
-//     float weight=0;
-//     struct edge nextEdge;
-
-//     nodes.erase(nodes.begin());
-
-//     while( nodes.size() )
-//     {
-//         weight=0;
-//         do
-//         {
-//             nextNode=nodeHeuristic(g, init.label, weight, solution, nodes);
-//             if( nextNode == -1) return vector<node>(); //NP if the heuristic can not find a node
-
-//             nextEdge=edge(init, nodes[nextNode], g.get_weight(init.label,nextNode));
-
-//             weight=nextEdge.weight;
-//         } while( cycle(solution, nextEdge) );
-
-        
-//         solution.push_back(nextEdge);
-
-//         init=nodes[nextNode];
-//         nodes.erase(nodes.begin()+nextNode);
-
-//     }
-
-
-//     vector<node> solution_;
-//     solution_.push_back(solution[0].p1);
-//     for(int i=0; i<solution.size(); i++) solution_.push_back(solution[i].p2);
-//     return solution_;
-// }
 
 vector<node> kruskal_heuristic2(graph &g, int i)
 {
