0f0a82966dd1c4771b540e33e174e3f30545b7e9
antoniogamiz <antoniogamiz10@gmail.com>
2018-04-25 11:19:22 +0200

    adaptacion a n-electricistas

--- Práctica 3/src/n-electrician.cpp
+++ Práctica 3/src/n-electrician.cpp
@@ -1,5 +1,8 @@
 
-//g++ ./src/electrician.cpp -o ./bin/electrician; ./bin/electrician ./datosTSP/att48.tsp > ./datosTSP/OPTIMOS.opt.tour
+
+/*-----------Solution with n-electricians---------*/
+
+//g++ ./src/n-electrician.cpp -o ./bin/n-electrician; ./bin/n-electrician ./datosTSP/att48.tsp n > ./datosTSP/OPTIMOS.opt.tour
 
 #include <iostream>
 #include <sstream>
@@ -91,35 +94,44 @@ int nodeHeuristic(graph &g, struct node n, float min_bound, vector<node> taken){
     return index_min;
 }
 
-vector<node> greedy(graph &g, int begin)
+vector<vector<node> > greedy(graph &g, int begin, int n_electricians)
 {
     vector<node> nodes=g.get_nodes();
-    vector<node> solution;
-    
+    vector<vector<node> > solution(n_electricians);
+    vector<node> taken;
+
+    // run ./datosTSP/att48.tsp 1
     int index_nextNode;
 
     struct node current_node=nodes[begin];
+    taken.push_back(current_node);
     bool first_time=true;
-    solution.push_back(current_node);
+    
+    for(int i=0; i<n_electricians; i++)
+        solution[i].push_back(current_node);
 
 
     struct node nextNode;
     first_time=true;
-    
-    while(solution.size() < nodes.size())
+    int j=0;
+    while( taken.size() < nodes.size() )
     {
+        if(j==n_electricians) j=0;
+        current_node=solution[j].back();
+        /*------------searching next node-----------*/
         first_time=true;
         do
         {
-            index_nextNode=nodeHeuristic(g, current_node, (first_time)? 0:g.get_weight(current_node.label, nextNode.label), solution);
-            if( index_nextNode==-1 ) return vector<node>();
+            index_nextNode=nodeHeuristic(g, current_node, (first_time)? 0:g.get_weight(current_node.label, nextNode.label), taken);
+            if( index_nextNode==-1 ) return vector<vector<node> >();
             nextNode=nodes[index_nextNode];
             
             first_time=false;
-        } while( cycle( solution, nextNode ) );
-    
-        solution.push_back(nextNode);
-        current_node=nextNode;
+        } while( cycle( solution[j], nextNode ) );
+        /*------------searching next node-----------*/
+        solution[j].push_back(nextNode);
+        taken.push_back(nextNode);
+        j++;
     }
 
     return solution;
@@ -135,15 +147,16 @@ float total_weight(graph &g, vector<node>& way)
 
 int main(int argc, char **argv)
 {
-    if( argc!=2 )
+    if( argc!=3 )
     {
         cerr << "Error: incorrect number of parameters. \n";
-        cerr << "Format: " << argv[0] << " <graph_file>\n";
+        cerr << "Format: " << argv[0] << " <graph_file> <number-electricians>\n";
         exit(1);
     }
 
     ifstream f(argv[1], ios::in);
-
+    int n=atoi(argv[2]);
+    
     if(!f)
     {
         cerr << "Error: couldnt open " << argv[1] << "\n";
@@ -154,7 +167,7 @@ int main(int argc, char **argv)
     graph G(dimension);
     fill_graph(G, f);
 
-    vector<node> way, minimal_way=greedy(G, 0);
+    vector<vector<node> > way, minimal_way=greedy(G, 0, n);
 
     /*------Uncomment to find the minimal way applyins the heuristic for each node in the graph-----*/
     // float min=LONG_MAX, weight;
@@ -175,7 +188,11 @@ int main(int argc, char **argv)
     {
         cout << "DIMENSION: " << dimension << endl;
         for(int i=0; i<minimal_way.size(); i++)
-            cout << minimal_way[i].label+1 << " " << minimal_way[i].coord.first << " " << minimal_way[i].coord.second << endl;
+        {
+            cout << "Camino del electricista " << i+1 << ":" << endl;
+            for(int j=0; j<minimal_way[i].size(); j++)
+                cout << minimal_way[i][j].label+1 << " " << minimal_way[i][j].coord.first << " " << minimal_way[i][j].coord.second << endl;
+        }
     } 
     else
     {
